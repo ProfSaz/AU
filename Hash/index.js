bd9a6a4c517aa3f073c933d3135c131f211a11b8
profsaz <sazprof@gmail.com>
2024-01-02 20:55:41 +0100

    hash

---
+++ Hash/index.js
@@ -0,0 +1,16 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+// the possible colors that the hash could represent
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+// given a hash, return the color that created the hash'
+function findColor(hash) {
+    return COLORS.find(x => toHex(sha256(utf8ToBytes(x))) === toHex(hash));
+
+    console.log("yes");
+}
+
+const osas = "yes";
+
+module.exports = findColor;
---
+++ Hash/test.js
@@ -0,0 +1,17 @@
+import {assert} from 'chai';
+import osas from '../index.js';
+import findColor from '../index.js';
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+describe('findColor', () => {
+    COLORS.forEach((color) => {
+        it(`should work for ${color}`, () => {
+            assert.equal(findColor(sha256(utf8ToBytes(color))), color);
+        });
+    });
+});
+
+console.log(osas);
