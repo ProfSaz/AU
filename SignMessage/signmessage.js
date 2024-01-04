6ab3efdcdabec4d51e575e8d71cdf3b043e0a872
profsaz <sazprof@gmail.com>
2024-01-04 22:46:17 +0100

    sign message

---
+++ SignMessage/signmessage.js
@@ -0,0 +1,11 @@
+import secp from "ethereum-cryptography/secp256k1.js"
+import hashMessage from "./hashmessage.js";
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const messageHash = hashMessage(msg);
+    return secp.sign(messageHash, PRIVATE_KEY, { recovered: true });
+}
+
+export default signMessage;
