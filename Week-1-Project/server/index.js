830c5a7c5533ebfd12e9f1167bf48a7413ee5e5c
profsaz <sazprof@gmail.com>
2024-01-15 00:05:53 +0100

    address update

--- Week-1-Project/server/index.js
+++ Week-1-Project/server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "04933f11f9080edefb426faf8d2f1bf0cdc97d0f7d4c73f64ee5e8cd0124ec9679de2aa9ce4ebacebad6d3c1ecfed17c97cf0b106c9d30f7dd9c821252fbc9a0d4": 100,
+  "04b07614389161f73b2b0fcbdb9bb430ec92f98a513bba2c356f57878f23c00405eeab7a46523d98eaa1a66c2141986702a3d731457bd072098d6379cee8c973bd": 50,
+  "049a760ca4c851ac00d46007cd454a386d79582bda847f1da0b0060e68d2e1c3c3b5ede6c010e146caa92311bfb122dd8ab039d988b9a2af91e2b11b71aed0188d": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
